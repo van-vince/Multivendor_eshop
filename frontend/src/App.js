109f8e0d98a9be0beb2fd692c84d8a38907f3941
van-vince <van.vince2@gmail.com>
2023-04-07 03:23:45 +0000

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
